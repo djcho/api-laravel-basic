bf48fa0d4905f8c77a2504e3f23b4e4ef53cf954
djcho <eowns36@gmail.com>
2024-01-22 14:55:43 +0900

    add user api : register, login, me

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,51 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\LoginUserRequest;
+use App\Http\Requests\RegisterUserRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
-    //
+    public function register(RegisterUserRequest $request){
+        //자동으로 검증하는 방법
+        $validatedData = $request->validated();
+
+        $user = User::create([
+            'name'=> $validatedData['name'],
+            'email'=> $validatedData['email'],
+            'password'=> bcrypt($validatedData['password']),
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return response()->json([
+            'message' => 'Successfully created user!',
+            'access_token'=> $token,
+            'token_type' => 'Bearer',
+            'user' => $user
+        ], 200);
+    }
+
+    public function login(LoginUserRequest $request){
+
+        $request->validated();
+
+        if(!Auth::attempt($request->only('email','password'))){
+            return response()->json([
+                'message' => 'Invalid login details'], 401);
+        }
+
+        $user = User::where('email', $request->email)->first();
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type'=> 'Bearer']);
+    }
+
+    public function me(Request $request){
+        return $request->user();
+    }
 }
---
+++ app/Http/Requests/RegisterUserRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        //권한이 있는가 검사하는 로직 추가될 수 있음
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name'=> 'required|string|max:255',
+            'email'=> 'required|string|email|max:255|unique:users',
+            'password'=> 'required|string|min:2',
+        ];
+    }
+}
